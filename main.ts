a8c9bb0288d530ea9bf92dcb695c938b02180733
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2019-12-21 08:04:44 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-
+function Fibonacci (num: number) {
+    x = 1
+    y = 1
+    t = num
+    while (t >= 3) {
+        nextTerm = x + y
+        t += -1
+        x = y
+        y = nextTerm
+    }
+    basic.showNumber(nextTerm)
+}
+let nextTerm = 0
+let t = 0
+let y = 0
+let x = 0
+Fibonacci(7)
+basic.forever(function () {
+	
+})
