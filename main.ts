3e04f3d3023fe425792d5a1d0db8f6d7b8bc73fa
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2019-12-21 08:39:34 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
+input.onButtonPressed(Button.AB, function () {
+    displayAnswer = true
+    Fibonacci(term)
+})
+input.onButtonPressed(Button.B, function () {
+    displayAnswer = false
+    term += 1
+    basic.showNumber(term)
+})
 function Fibonacci (num: number) {
+    basic.showString("?")
     x = 1
     y = 1
     t = num
@@ -8,13 +18,25 @@ function Fibonacci (num: number) {
         x = y
         y = nextTerm
     }
-    basic.showNumber(nextTerm)
+    while (displayAnswer == true) {
+        basic.showString("=")
+        basic.pause(100)
+        basic.showNumber(nextTerm)
+    }
 }
+input.onButtonPressed(Button.A, function () {
+    displayAnswer = false
+    if (term > 3) {
+        term += -1
+    }
+    basic.showNumber(term)
+})
 let nextTerm = 0
 let t = 0
 let y = 0
 let x = 0
-Fibonacci(7)
-basic.forever(function () {
-	
-})
+let displayAnswer = false
+let term = 0
+term = 3
+basic.showNumber(term)
+displayAnswer = false
